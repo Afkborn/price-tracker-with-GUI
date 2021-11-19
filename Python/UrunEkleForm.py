e5a349bccc4c828345fca12f1c64f0aad646ad8a
Bilgehan K <kalaybilgehan60@gmail.com>
2021-11-19 17:31:09 +0300

    version 1

--- Python/UrunEkleForm.py
+++ Python/UrunEkleForm.py
@@ -1,16 +1,20 @@
 from PyQt5 import QtWidgets
 from Python.Design.UrunEkleFormUI import Ui_UrunEkleForm
 from Python.Model.Product import Product
-from time import strptime
+from time import strptime, time
 from datetime import datetime
 from urllib.parse import urlparse
+from Python.Business.DatabaseProduct import DatabaseProduct
 
 class UrunEkleForm(QtWidgets.QMainWindow,Ui_UrunEkleForm):
+    URUN_EKLENDI_MI = False
+    BROWSER_GELDI = False
     def __init__(self, parent=None):
         super(UrunEkleForm, self).__init__(parent)
         self.setupUi(self)
         self.urun_ekle_button.clicked.connect(self.btnUrunEkle_click)
         self.urun_url_edit.textChanged.connect(self.url_textChanged_listener)
+        self.databaseProduct = DatabaseProduct()
 
     def getDomainFromURL(self,url:str) -> str:
         """Verilen str türündeki URL adresinin hangi domaine ait olduğunu döner"""
@@ -20,21 +24,36 @@ class UrunEkleForm(QtWidgets.QMainWindow,Ui_UrunEkleForm):
         if len(args[0]) > 0:
             self.urun_domain_edit.setText(self.getDomainFromURL(args[0]))
 
+    def getBrowser(self,browser):
+        self.browser = browser
+        self.BROWSER_GELDI = True
+    
+
 
     def btnUrunEkle_click(self):
-        urun_isim = self.urun_ismi_edit.text()
+        urun_isim = self.urun_ismi_edit.text().strip()
         urun_url = self.urun_url_edit.text()
         urun_domain = self.getDomainFromURL(urun_url)
         kontrol_time  = self.urun_kontrol_suresi_time.time().toPyTime()
         kontrol_time_sec = kontrol_time.hour * 3600 + kontrol_time.minute * 60 + kontrol_time.second
         urun_fiyat_takip = self.urun_fiyat_takip_cb.isChecked()
         urun_stok_takip = self.urun_stok_takip_cb.isChecked()
-        # ürünün değerlerini kontrol et
+        #TODO ürünün değerlerini kontrol et
+        
         self.urun_domain_edit.setText(self.getDomainFromURL(self.urun_url_edit.text()))
         myProduct = Product(isim=urun_isim,link=urun_url,check_time_sec=kontrol_time_sec,fiyat_takip=urun_fiyat_takip,stok_takip=urun_stok_takip,domain=urun_domain)
-        print(myProduct)
-        #TODO ürün verilerini internetten kontrol et
-        #TODO ürünün veritabanına kaydedilmesi
-        #TODO ürün ekleme formu kapansın
-        #TODO ürünler listesine yeni ürünü ekle
+        myProduct.set_son_kontrol_zamani(time())
+
+        if self.BROWSER_GELDI:
+            price = self.browser.getPriceFromProduct(myProduct)
+            stock = self.browser.getStockFromProduct(myProduct)
+            myProduct.set_fiyat(price)
+            myProduct.set_stok(stock)
+
+
+
+        self.databaseProduct.addProduct(myProduct)
+        self.URUN_EKLENDI_MI = True
+        
+        self.close()
 
--- Python/UrunlerWindow.py
+++ Python/UrunlerWindow.py
@@ -1,10 +1,13 @@
 
 from sys import argv
 from os import getcwd
+from time import strftime,time
+from datetime import date, datetime
+
+
+#PyQT5
 from PyQt5 import QtWidgets, uic
-
-
-from PyQt5.QtCore import QTimer #timer
+from PyQt5.QtCore import QTime, QTimer #timer
 from PyQt5.QtTest import QTest # qsleep 
 
 
@@ -15,18 +18,47 @@ from Python.HakkimdaForm import HakkımdaForm
 from Python.UrunEkleForm import UrunEkleForm
 from Python.UrunDetayForm import UrunDetayForm
 
+#Business
+from Python.Business.DatabaseProduct import DatabaseProduct
+from Python.Business.WebTracker import WebPrice
 
 #MODEL
 from Python.Model.Product import Product
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_UrunlerWindow):
+    __productList = list()
+
+    #isim fiyat_takip stok_takip fiyat stok son_kontrol_zamani link
+    __table_widget_name = ["İsim","Fiyat Takip","Stok Takip","Fiyat","Stok Durumu","Son Kontrol Zamanı","Link"]
+
     def __init__(self, *args, obj=None, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self) 
-        
+
+        #NOTIFY PRODUCT LIST CHANGED TIMER
+        self.notify_product_list_changed_timer = QTimer()
+        self.notify_product_list_changed_timer.timeout.connect(self.notify_product_list_changed)
+        self.notify_product_list_changed_timer.start(1000)
+
+        #kontrol zamanı gelip gelmediğpini kontrol eden timer
+        self.kontrol_zamanı_timer = QTimer()
+        self.kontrol_zamanı_timer.timeout.connect(self.kontrol_zamani_kontrol_et)
+        self.kontrol_zamanı_timer.start(1000)
+
+
+        #DATABASE
+        self.databaseProduct = DatabaseProduct()
+
+        #WEB
+        self.web_browser = WebPrice("profile1")
+
+
+        #FORMS
         self.ayarlarForm = AyarlarForm(self)
         self.urunEkleForm = UrunEkleForm(self)
+        self.urunEkleForm.getBrowser(self.web_browser)
+
         self.hakkimdaForm = HakkımdaForm(self)
         self.urunDetayForm = UrunDetayForm(self)
 
@@ -34,8 +66,15 @@ class MainWindow(QtWidgets.QMainWindow, Ui_UrunlerWindow):
         self.actionCikis.triggered.connect(self.close)
         self.actionHakkimda.triggered.connect(self.showAboutWindow)
         self.actionYeni_Urun.triggered.connect(self.showNewProductWindow)
+        
+        self.load_products_from_sqlite()
+        self.load_product_table_attributes()
+        self.load_product_from_product_list()
 
-        myProduct = Product(id=1,isim="aaa",link="https://market.samm.com/raspberry-pi-zero-2-w",check_time_sec=600,fiyat_takip=True,stok_takip=True,fiyat=18.99,stok=True,son_kontrol_zamani=None,domain="market.samm.com")
+
+
+
+        myProduct = self.createTestProduct()
         self.detay_button.clicked.connect(lambda: self.showProductDetailWindowWithProduct(myProduct))
         
 
@@ -54,6 +93,102 @@ class MainWindow(QtWidgets.QMainWindow, Ui_UrunlerWindow):
         self.urunDetayForm.setProduct(product)
         self.urunDetayForm.loadProduct()
 
+    def createTestProduct(self) -> Product:
+        myProduct = Product(id=1,isim="aaa",link="https://market.samm.com/raspberry-pi-zero-2-w",check_time_sec=600,fiyat_takip=True,stok_takip=True,fiyat=18.99,stok=True,son_kontrol_zamani=1637312657.6746655,domain="market.samm.com")
+        # self.__productList.append(myProduct)
+        return myProduct
+
+    def load_product_table_attributes(self):
+        self.product_table_widget.setRowCount(len(self.__productList))
+
+        self.product_table_widget.setColumnCount(len(self.__table_widget_name))
+        self.product_table_widget.setHorizontalHeaderLabels(self.__table_widget_name)
+
+        self.product_table_widget.horizontalHeader().setStretchLastSection(True)
+        self.product_table_widget.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Interactive)
+
+    def load_products_from_sqlite(self):
+        """SQLite'den ürünleri çek"""
+        self.__productList.clear()
+        self.product_table_widget.clear()
+        self.__productList = self.databaseProduct.loadDB()
+
+    def notify_product_list_changed(self, control = False):
+        if self.urunEkleForm.URUN_EKLENDI_MI:
+            self.load_products_from_sqlite()
+            self.load_product_table_attributes()
+            self.load_product_from_product_list()
+            self.urunEkleForm.URUN_EKLENDI_MI = False
+        elif control:
+            self.load_products_from_sqlite()
+            self.load_product_table_attributes()
+            self.load_product_from_product_list()
 
 
+    def kontrol_zamani_kontrol_et(self):
+        for index,product in enumerate(self.__productList):
+            guncelTime = time()
+            if product.get_son_kontrol_zamani() + product.get_check_time_sec() < guncelTime:
+                print(f"güncellemesi gelmiş  {product.get_isim()}")
+                
+                #TODO GÜNCELLEMESİ YAP
+                price = self.web_browser.getPriceFromProduct(product)
+                stock = self.web_browser.getStockFromProduct(product)
+                product.set_fiyat(price)
+                product.set_stok(stock)
+                product.set_son_kontrol_zamani(time())
+
+
+                #TODO DATABASE PRODUCT UPDATE FONKSİYONU YAZ
+                self.databaseProduct.updatePriceWithID(product.get_id(),product.get_fiyat())
+                
+                self.databaseProduct.updateStockWithId(product.get_id(),product.get_stok())
+                self.databaseProduct.updateSonKontrolZamaniWithId(product.get_id(),product.get_son_kontrol_zamani())
+                
+                self.notify_product_list_changed(control=True)
+                
+            else:
+                print(f"güncellemesi gelmemiş.{product.get_isim()}")
+
+
+
+
+
+        
+
+    def printTest(self,test):
+        print(test)
+
+    def load_product_from_product_list(self):
+        """Productları table widget'a yükle"""
+        for index,product in enumerate(self.__productList):
+            #isim fiyat_takip stok_takip fiyat stok son_kontrol_zamani link
+            self.product_table_widget.setItem(index,0,QtWidgets.QTableWidgetItem(product.get_isim()))
+            if product.get_fiyat_takip():
+                self.product_table_widget.setItem(index,1,QtWidgets.QTableWidgetItem("Evet"))
+            else:
+                self.product_table_widget.setItem(index,1,QtWidgets.QTableWidgetItem("Hayır"))
+            if product.get_stok_takip():
+                self.product_table_widget.setItem(index,2,QtWidgets.QTableWidgetItem("Evet"))
+            else:
+                self.product_table_widget.setItem(index,2,QtWidgets.QTableWidgetItem("Hayır"))
+
+
+            if product.get_fiyat() == None:
+                self.product_table_widget.setItem(index,3,QtWidgets.QTableWidgetItem("None"))
+            else:
+                self.product_table_widget.setItem(index,3,QtWidgets.QTableWidgetItem(str(product.get_fiyat())))
+
+
+            if product.get_stok():
+                self.product_table_widget.setItem(index,4,QtWidgets.QTableWidgetItem("Var"))
+            elif product.get_stok() == False:
+                self.product_table_widget.setItem(index,4,QtWidgets.QTableWidgetItem("Yok"))
+            else:
+                self.product_table_widget.setItem(index,4,QtWidgets.QTableWidgetItem("None"))
+
+            my_time = datetime.fromtimestamp(product.get_son_kontrol_zamani()).strftime(' %H:%M:%S %Y-%m-%d')
+
+            self.product_table_widget.setItem(index,5,QtWidgets.QTableWidgetItem(my_time))
+            self.product_table_widget.setItem(index,6,QtWidgets.QTableWidgetItem(product.get_link()))
 
